c0f2423c4b08576f899b3cefb35d262b853241ab
seethaler <jake@userlite.com>
2015-08-04 19:09:07 -0600

    First Save

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,9 +1,35 @@
 var app = angular.module('chatroom');
 
 app.controller('mainCtrl', function($scope, parseService){
+
+  parseService.getData()
+  .success(function (data) {
+    $scope.messages = data.results
+  })
+
+  parseService.getDataId()
+  .success(function (data) {
+    $scope.ids = data.results
+  })
+
+  $scope.postData = function() {
+    parseService.postData($scope.message)
+    .success(function () {
+      $scope.message = '';
+      parseService.manipulateData()
+      .success(function (data) {
+        $scope.messages = data.results
+      })
+
+    })
+  }
+
+  })
+
+
   //In your controller you'll have a getParseData function and a postData function, but should be placed on $scope.
 
-  //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
+  //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to
   //your controllers $scope as messages ($scope.messages)
 
 
@@ -19,4 +45,3 @@ app.controller('mainCtrl', function($scope, parseService){
   // setInterval(function(){
   //   $scope.getParseData();
   // }, 1500)
-})
